d7079036ab1c95019659667a279199a180b575d0
alexpachvtb <alexpachvtb@gmail.com>
2021-09-27 16:31:57 +0300

    feat: add javascript

--- museum/js/index.js
+++ museum/js/index.js
@@ -1,10 +1,3 @@
-const rangeAll = document.querySelectorAll('.video__range');
-function changeRange(i) {
-  let value = rangeAll[i].value;
-  rangeAll[i].style.cssText = `background: -webkit-linear-gradient(left, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%);`
-}
-rangeAll.forEach((item, i) => {
-  item.addEventListener('input', () => {
-    changeRange(i);
-  });
-});
+import * as video from './modules/video.js';
+import * as tickets from './modules/tickets.js';
+import * as gallery from './modules/gallery.js';
