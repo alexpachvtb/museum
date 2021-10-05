caf1b57343c04691dac2437aaa0e11a97f90bfb3
alexpachvtb <alexpachvtb@gmail.com>
2021-10-05 23:34:47 +0300

    docs: delete self-esteem index.js

--- museum/js/index.js
+++ museum/js/index.js
@@ -3,5 +3,3 @@ import * as tickets from './modules/tickets.js';
 import * as gallery from './modules/gallery.js';
 import * as form from './modules/form.js';
 import * as menu from './modules/menu.js';
-
-console.log('Моя оценка пока 148 баллов, так как задание еще в процессе доработки. За форму покупки билетов 12 баллов из 22 возможных -10 за несоответствие макету, и -2 балла за отсутствие ripple-эффекта на кнопке buy now');
