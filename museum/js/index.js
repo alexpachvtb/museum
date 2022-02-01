29c9ec7ad34722f3ab31b75d07d8fc81e802d646
Alexander Pachkovsky <alexpachvtb@gmail.com>
2022-02-01 11:16:39 +0300

    Delete museum directory

--- museum/js/index.js
+++
@@ -1,6 +0,0 @@
-import * as video from './modules/video.js';
-import * as tickets from './modules/tickets.js';
-import * as gallery from './modules/gallery.js';
-import * as form from './modules/form.js';
-
-console.log('Моя оценка пока 140 баллов, так как задание еще в процессе доработки. За форму покупки билето 4 балла из 22 возможных, и -2 балла за отсутствие ripple-эффекта на кнопке buy now');
